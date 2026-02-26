89ec6071b62a6534d70ac85f86e492a04d5ffc91
KarthikeyaPatrudu <152089927+KarthikeyaPatrudu@users.noreply.github.com>
2026-02-26 16:05:22 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,1 +1,51 @@
 
+├── 📁 backend
+│   ├── 📁 models
+│   │   └── 📄 TollLog.js
+│   ├── 📄 importExcel.js
+│   ├── ⚙️ package-lock.json
+│   ├── ⚙️ package.json
+│   └── 📄 server.js
+├── 📁 frontend
+│   ├── 📁 public
+│   │   └── 🖼️ vite.svg
+│   ├── 📁 src
+│   │   ├── 📁 app
+│   │   │   ├── 📄 App.jsx
+│   │   │   ├── 📄 main.jsx
+│   │   │   └── 📄 store.js
+│   │   ├── 📁 assets
+│   │   │   └── 🖼️ CHOLAFIN.NS.png
+│   │   ├── 📁 components
+│   │   │   └── 📁 Map
+│   │   │       └── 📄 MapView.jsx
+│   │   ├── 📁 features
+│   │   │   ├── 📁 auth
+│   │   │   │   ├── 📄 LoginPage.jsx
+│   │   │   │   ├── 📄 authService.js
+│   │   │   │   └── 📄 authSlice.js
+│   │   │   ├── 📁 route
+│   │   │   │   ├── 📄 ExcludedPanel.jsx
+│   │   │   │   ├── 📄 SearchPanel.jsx
+│   │   │   │   ├── 📄 routeService.js
+│   │   │   │   └── 📄 routeSlice.js
+│   │   │   ├── 📁 tracker
+│   │   │   │   ├── 📄 trackerSelectors.js
+│   │   │   │   └── 📄 trackerSlice.js
+│   │   │   └── 📁 vehicle
+│   │   │       ├── 📄 vehicleService.js
+│   │   │       └── 📄 vehicleSlice.js
+│   │   ├── 📁 pages
+│   │   │   ├── 📄 TrackerPage.jsx
+│   │   │   └── 📄 VehicleDashboard.jsx
+│   │   └── 📁 styles
+│   │       ├── 🎨 app.css
+│   │       └── 🎨 login.css
+│   ├── ⚙️ .gitignore
+│   ├── 📝 README.md
+│   ├── 📄 eslint.config.js
+│   ├── 🌐 index.html
+│   ├── ⚙️ package-lock.json
+│   ├── ⚙️ package.json
+│   └── 📄 vite.config.js
+└── ⚙️ .gitignore
