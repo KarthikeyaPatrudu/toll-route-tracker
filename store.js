af97153f5d2204962cb4d6f02eeae90e2c48f02b
KarthikeyaPatrudu <152089927+KarthikeyaPatrudu@users.noreply.github.com>
2026-02-27 15:02:28 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,15 +1,1 @@
-const parseLocation = (locString) => {
-  if (!locString) return null;
 
-  const parts = locString.trim().split(/\s+/);
-
-  if (parts.length !== 2) return null;
-
-  const lng = Number(parts[0]);
-  const lat = Number(parts[1]);
-
-  // 🚨 guard against bad data
-  if (isNaN(lat) || isNaN(lng)) return null;
-
-  return [lat, lng]; // ✅ Leaflet format
-};
