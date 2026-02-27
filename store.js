e35162c12861388c9fd0bf1b55fa8870b4e14cdc
KarthikeyaPatrudu <152089927+KarthikeyaPatrudu@users.noreply.github.com>
2026-02-27 15:42:46 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,2 +1,3 @@
-
-pls remove the markers i only want route to be seen
+i want to add markers but if we add markers for every request rendering problem occurs .
+  suggest me a method by which we can add marker.
+  but i dont want markers for evry request , i only wnat those many where the brower can render peacfully
